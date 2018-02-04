7fec276a04814cd6480128dddabbfc5452f0d9aa
kondeichmann <k.deichmann@mac.com>
2018-02-04 18:03:38 +0100

    make last design changes

--- Decrypt/routes/index.js
+++ Decrypt/routes/index.js
@@ -7,73 +7,42 @@ var openpgp = require('openpgp');
 openpgp.initWorker({ path:'openpgp.worker.js' })
 openpgp.config.aead_protect = true
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
 
 	var query = req.query
 
-	if (!query.text) {
-		res.render('parameterless', { 
-  			title: 'Enzevalos',
-  			messageTitle: 'Seite nicht gefunden',
-  			additional: 'Versuche es nocheinmal mit dem Link aus der E-Mail'
-  		});
+	if (!query.text || !query.cipher) {
 
+		res.redirect("http://userpage.fu-berlin.de/wieseoli/letterbox/");
   		return
 	}
 
 	let texts = query.text.split(',')
 	
-	res.render('index', {title: 'Entschlüsseln', texts: texts})
+	res.render('index', {title: 'Entschlüsseln', texts: texts, cipher: query.cipher, stored_texts: texts.join(',')})
 });
 
-router.get('/decrypt', function(req, res, next) {
+router.post('/decrypt', function(req, res) {
 
-	//-----BEGIN PGP MESSAGE-----\n\nww0EBwMIT2tdgUK9LBfX0kEBrzkG4ELPGzfGVoNsD4l8W7X4IORgud5tkalTpVrL+hq7fKdwt53i\nyD2fmQjDt45JSAOKOBftAR6EmRCvuB048g==\n=ak/6\n-----END PGP MESSAGE-----\n
-	// 7505-3824
-
-	//ww0EBwMIZoDGErw5YU7X0kEBrk+dC7fFAgy2SyBa7tA9cw++khbA1FtzBsCLuyidc2LBJ/2fqqV7ak1kwBNpTv5AuYvAXgWv29EYw+pNwS5dGw==
-	//0661-4928
-
-	var message = openpgp.message.fromText("ww0EBwMIZoDGErw5YU7X0kEBrk+dC7fFAgy2SyBa7tA9cw++khbA1FtzBsCLuyidc2LBJ/2fqqV7ak1kwBNpTv5AuYvAXgWv29EYw+pNwS5dGw==")
-	var armor = message.armor()
-	console.log(armor)
+	console.log(req.body)
 
 	var options = {
-	    message: openpgp.message.readArmored(armor),
-	    password: '0661-4928'
+	    message: openpgp.message.readArmored(req.body.cipher),
+	    password: req.body.password.trim()
 	};
 
-	console.log(new TextDecoder("ascii").decode(options.message.packets.write()))
-	openpgp.decrypt(options).then(function(plaintext) {
-		console.log(plaintext)
-	    res.send(plaintext.data)
-	})
-});
+	openpgp.decrypt(options)
+		.then(function(plaintext) {
 
-router.get('/encrypt', function(req, res, next) {
-	var options, encrypted;
-
-	options = {
-	    data: "Kontonummer",
-	    passwords: '9198-4099',
-	    armor: false
-	};
-	
-	openpgp.encrypt(options).then(function(ciphertext) {
-		console.log(new TextDecoder("utf-8").decode(ciphertext.message.packets.write()));
-		var array = ciphertext.message.packets.write();
-
-		options = {
-	    	message: openpgp.message.read(array),
-	    	password: '9198-4099'
-		};
-
-		openpgp.decrypt(options).then(function(plaintext) {
-			console.log(plaintext)
-	  		res.send(plaintext.data)
+	    	res.render('decrypt', { 
+  				title: 'Klartext',
+  				texts: plaintext.data.split('\n')
+  			});
 		})
-	});
+		.catch(function(reason) {
+			
+			res.render('index', {title: 'Entschlüsseln', texts: req.body.texts.split(','), cipher: req.body.cipher, password: req.body.password, isInvalid: "is-invalid"})
+		})
 });
 
 module.exports = router;
